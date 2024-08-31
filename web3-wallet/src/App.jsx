5f98c56a053173b715dda10da7ae9dda8c388312
Akash-YS05 <akash.pan05@gmail.com>
2024-08-31 19:51:44 +0530

    creating solana wallet

--- web3-wallet/src/App.jsx
+++ web3-wallet/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { generateMnemonic } from "bip39";
 
-import './App.css'
+import "./App.css";
 
 function App() {
+  const [mnemonic, setMnemonic] = useState("");
 
   return (
     <>
       <h1>- Web3 Wallet -</h1>
+      <input type="text" value={mnemonic} />
+      <button onClick={async function() {
+        const mn = generateMnemonic();
+        setMnemonic(mn)
+      }}>Generate Seed Phrase</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
