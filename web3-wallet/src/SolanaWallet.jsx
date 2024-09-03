88990de390aebad9a195fcada62a62d15f7234c3
Akash-YS05 <akash.pan05@gmail.com>
2024-09-03 23:50:13 +0530

    check balance (sol)

--- web3-wallet/src/SolanaWallet.jsx
+++ web3-wallet/src/SolanaWallet.jsx
@@ -3,10 +3,43 @@ import { mnemonicToSeed } from "bip39";
 import { derivePath } from "ed25519-hd-key";
 import { Keypair } from "@solana/web3.js";
 import nacl from "tweetnacl";
+import axios from "axios";
 
 export function SolanaWallet({mnemonic}) {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [publicKeys, setPublicKeys] = useState([]);
+    const [currentBalance, setCurrentBalance] = useState({});
+
+
+    const checkBalance = async (address) => {
+        try {
+            const alchemy_url = 'https://solana-mainnet.g.alchemy.com/v2/CGikAm9Fk05kPDMVhvoRQ_LP6GIEtVj9';
+            const body = {
+                jsonrpc: '2.0',
+                id: 1,
+                method: 'getBalance',
+                params: [address]
+            };
+            const response = await axios.post(alchemy_url, body);
+
+            if (response.data.error) {
+                console.error('Error fetching balance:', response.data.error);
+                alert("ERROR OCCURED: " + response.data.error)
+            } else {
+                const balance = response.data.result.value;
+                setCurrentBalance((prevBalance) => ({
+                    ...prevBalance,
+                    [address] : balance.toString()
+                }))
+            }
+
+
+
+        } catch(e) {
+            console.log("Error", e)
+        }
+    }
+
 
     return <div>
         <button onClick={function() {
@@ -20,8 +53,15 @@ export function SolanaWallet({mnemonic}) {
         }}>
             Add Solana Wallet
         </button>
-        {publicKeys.map((p, idx) => <div style={{textAlign:'left'}}>
+        {publicKeys.map((p, idx) => <div key={idx} style={{textAlign:'left'}}>
             Wallet {idx + 1} - {p.toBase58()}
+            <br />
+            <button style={{backgroundColor: 'white', color: 'black', textDecoration: 'underline'}} onClick={() => {checkBalance(p)}}>
+                Check Balance
+            </button>
+            <span>
+                Current Balance = {currentBalance[p]}
+            </span>
         </div>)}
     </div>
 }
